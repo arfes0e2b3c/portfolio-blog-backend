30ca16d3760a03f34d649a0c74633e1b7aec98c1
arfes0e2b3c <webyata@gmail.com>
2024-10-23 15:50:15 +0900

    update: dbへの接続を共通化

--- src/repository/category.ts
+++ src/repository/category.ts
@@ -1,62 +1,74 @@
 import { sql } from 'drizzle-orm'
 import type { CategoryInputSchema } from '../../openapi/category'
-import { db } from '../db/db'
+import { withDbConnection } from '../db'
 import { categoriesTable } from '../db/schema'
 
 class CategoryRepository {
 	async getAll() {
-		return await db
-			.select()
-			.from(categoriesTable)
-			.where(sql`${categoriesTable.deletedAt} IS NULL`)
+		return withDbConnection(async (db) => {
+			return await db
+				.select()
+				.from(categoriesTable)
+				.where(sql`${categoriesTable.deletedAt} IS NULL`)
+		})
 	}
 
 	async findByName(name: string) {
-		return await db
-			.select()
-			.from(categoriesTable)
-			.where(
-				sql`${categoriesTable.name} = ${name} and ${categoriesTable.deletedAt} IS NULL`
-			)
+		return withDbConnection(async (db) => {
+			return await db
+				.select()
+				.from(categoriesTable)
+				.where(
+					sql`${categoriesTable.name} = ${name} and ${categoriesTable.deletedAt} IS NULL`
+				)
+		})
 	}
 
 	async findById(categoryId: string) {
-		return await db
-			.select()
-			.from(categoriesTable)
-			.where(
-				sql`${categoriesTable.id} = ${categoryId} and ${categoriesTable.deletedAt} IS NULL`
-			)
+		return withDbConnection(async (db) => {
+			return await db
+				.select()
+				.from(categoriesTable)
+				.where(
+					sql`${categoriesTable.id} = ${categoryId} and ${categoriesTable.deletedAt} IS NULL`
+				)
+		})
 	}
 
 	async create(body: CategoryInputSchema) {
-		const res = await db
-			.insert(categoriesTable)
-			.values(body)
-			.returning({ id: categoriesTable.id })
-		return res[0]
+		return withDbConnection(async (db) => {
+			const res = await db
+				.insert(categoriesTable)
+				.values(body)
+				.returning({ id: categoriesTable.id })
+			return res[0]
+		})
 	}
 
 	async updateById(body: CategoryInputSchema, categoryId: string) {
-		const res = await db
-			.update(categoriesTable)
-			.set({ updatedAt: sql`NOW()`, ...body })
-			.where(
-				sql`${categoriesTable.id} = ${categoryId} and ${categoriesTable.deletedAt} IS NULL`
-			)
-			.returning({ id: categoriesTable.id })
-		return res[0]
+		return withDbConnection(async (db) => {
+			const res = await db
+				.update(categoriesTable)
+				.set({ updatedAt: sql`NOW()`, ...body })
+				.where(
+					sql`${categoriesTable.id} = ${categoryId} and ${categoriesTable.deletedAt} IS NULL`
+				)
+				.returning({ id: categoriesTable.id })
+			return res[0]
+		})
 	}
 
 	async deleteById(categoryId: string) {
-		const res = await db
-			.update(categoriesTable)
-			.set({ deletedAt: sql`NOW()`, updatedAt: sql`NOW()` })
-			.where(
-				sql`${categoriesTable.id} = ${categoryId} and ${categoriesTable.deletedAt} IS NULL`
-			)
-			.returning({ id: categoriesTable.id })
-		return res[0]
+		return withDbConnection(async (db) => {
+			const res = await db
+				.update(categoriesTable)
+				.set({ deletedAt: sql`NOW()`, updatedAt: sql`NOW()` })
+				.where(
+					sql`${categoriesTable.id} = ${categoryId} and ${categoriesTable.deletedAt} IS NULL`
+				)
+				.returning({ id: categoriesTable.id })
+			return res[0]
+		})
 	}
 }
 
